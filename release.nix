60e84085ce7742053725f582aa38e3ac06386e7c
Jacek Galowicz <jacek@galowicz.de>
2020-03-24 11:12:43 +0100

    Enable recursive nix-build

--- release.nix
+++ release.nix
@@ -1,30 +1,34 @@
 with import <nixpkgs> {};
 
 rec {
-  a = stdenv.mkDerivation {
-    name = "liba";
-    buildInputs = [ c d ];
-    nativeBuildInputs = [ cmake ];
-    src = ./a;
-  };
-  b = stdenv.mkDerivation {
-    name = "libb";
-    nativeBuildInputs = [ cmake ];
-    src = ./b;
-  };
-  c = stdenv.mkDerivation {
-    name = "libc";
-    nativeBuildInputs = [ cmake ];
-    src = ./c;
-  };
-  d = stdenv.mkDerivation {
-    name = "libd";
-    nativeBuildInputs = [ cmake ];
-    src = ./d;
+  recurseForDerivations = true;
+  lib = rec {
+    recurseForDerivations = true;
+    a = stdenv.mkDerivation {
+      name = "liba";
+      buildInputs = [ c d ];
+      nativeBuildInputs = [ cmake ];
+      src = ./a;
+    };
+    b = stdenv.mkDerivation {
+      name = "libb";
+      nativeBuildInputs = [ cmake ];
+      src = ./b;
+    };
+    c = stdenv.mkDerivation {
+      name = "libc";
+      nativeBuildInputs = [ cmake ];
+      src = ./c;
+    };
+    d = stdenv.mkDerivation {
+      name = "libd";
+      nativeBuildInputs = [ cmake ];
+      src = ./d;
+    };
   };
   myapp = stdenv.mkDerivation {
     name = "myapp";
-    buildInputs = [ a b c d ];
+    buildInputs = with lib; [ a b c d ];
     nativeBuildInputs = [ cmake ];
     src = ./app;
   };
