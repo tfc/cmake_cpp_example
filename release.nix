37662a5d8f3375a27948676567eda351325148d2
Jacek Galowicz <jacek@galowicz.de>
2023-12-05 20:04:08 +0100

    Solution part 1 without overlays

--- release.nix
+++ release.nix
@@ -11,6 +11,24 @@ in
     libc
     libd
     myapp;
+
+  myapp-patched =
+    let
+      patchIt = drv: inStr: outStr: drv.overrideAttrs (_: {
+        patchPhase = ''
+          substituteInPlace main.cpp \
+            --replace '"${inStr}"' '"${outStr}"'
+        '';
+      });
+    in
+    pkgs.myapp.override {
+      liba = (patchIt pkgs.liba "a" "x").override {
+        libc = patchIt pkgs.libc "C" "z";
+        libd = patchIt pkgs.libd "d" "w";
+      };
+      libb = patchIt pkgs.libb "b" "y";
+    };
+
   myapp-static = pkgs.pkgsStatic.myapp;
   myapp-win64 = pkgs.pkgsCross.mingwW64.myapp;
   myapp-aarch64 = pkgs.pkgsCross.aarch64-multiplatform.myapp;
